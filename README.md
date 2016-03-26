0a1c369ad6e9100c31468eb5af000a8f53361426
Markus Hafellner <markus.hafellner@gmail.com>
2016-03-27 00:06:08 +0100

    Add Implementation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Project Euler Problem 1
-Solution for Project Euler Problem 1 written in Scala. Link to the Problem: [Problem 1](https://projecteuler.net/problem=1)
+Solution for Project Euler Problem 1 written in Scala. 
+
+Problem description:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+Link to the Problem: [Problem 1](https://projecteuler.net/problem=1)
--- src/ScalaApp.scala
+++ src/ScalaApp.scala
@@ -1,8 +1,42 @@
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by Markus on 25/03/2016.
   */
 object ScalaApp {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val allMod3 = findAllMod3(100)
+    val allMod5 = findAllMod5(100)
+    val sum = sumArrays(allMod3, allMod5)
+    println(sum)
+  }
+
+  def findAllMod3(limit: Int): Array[Int] = {
+    val allMultiples = new ArrayBuffer[Int]()
+    for (i <- 1 until limit) {
+      if (i % 3 == 0) {
+        allMultiples += i
+      }
+    }
+    allMultiples.toArray
+  }
+
+  def findAllMod5(limit: Int): Array[Int] = {
+    val allMultiples = new ArrayBuffer[Int]()
+    for (i <- 1 until limit) {
+      if (i % 5 == 0) {
+        allMultiples += i
+      }
+    }
+    allMultiples.toArray
+  }
+
+  def sumArrays(a: Array[Int], b: Array[Int]): Int = {
+    var sum: Int = 0
+    val array = a.union(b).distinct
+    for (i <- array.indices) {
+      sum += array(i)
+    }
+    sum
   }
 }
